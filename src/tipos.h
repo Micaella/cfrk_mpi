3a64c777d5b0cd49954b4b13cc050dca8c4563b9
Micaella <miicac@hotmail.com>
2018-12-05 19:50:58 -0200

    Update tipos.h

--- src/tipos.h
+++ src/tipos.h
@@ -12,26 +12,18 @@ typedef unsigned int uint;
 //0 disabled; 1 enable
 const int DBG = 0;
 
-struct dic// Dictionary
-{
-   int *index;// Index of combination
-   int *freq;// Frequency of repetition
-   int *id_pos;// Index on the pos vector
-   int *pos;// Where index appears
-};
-
 struct read// Used to read sequences
 {
-   short *data;
+   char *data;
    int *length;
-   int *start;
+   lint *start;
    struct read *next;
 };
 
 struct tmp_data// Used to auxiliate the struct read
 {
-   short *data;
-   uint length;
+   char *data;
+   int length;
    struct tmp_data *next;
 };
 
