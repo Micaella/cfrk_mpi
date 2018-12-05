0f0b9f7b6ce382ef8dcbb9a514b76466090becfc
Micaella <miicac@hotmail.com>
2018-12-05 19:37:57 -0200

    Update kmer.cuh

--- src/kmer.cuh
+++ src/kmer.cuh
@@ -3,16 +3,16 @@
 
 #include "tipos.h"
 
-int cfrk(char *dataset, char *outputfile, int k, lint chunkSize);
+int cfrk(char *dataset, char *outputfile, int k, lint chunkSize, int rank, int tambuf);
 
 void kmer_main(struct read *rd, FILE *outputfile, lint nN, lint nS, int k, ushort device);
 
 __global__ void SetMatrix(int *Mat, ushort offset, int val, int nF);
 
-__global__ void ComputeIndex(short *Seq, int *Index, const int k, lint nN, ushort offset);
+__global__ void ComputeIndex(char *Seq, int *Index, const int k, lint nN, ushort offset);
 
-__global__ void ComputeFreq(int *Index, int *Freq, int *start, int *length, ushort offset, int fourk, lint nS, lint nN);
+__global__ void ComputeFreq(int *Index, int *Freq, lint *start, int *length, ushort offset, int fourk, lint nS, lint nN);
 
-__global__ void ComputeFreqNew(int *Index, int *Freq, int *start, int *length, ushort offset, int fourk, lint nS);
+__global__ void ComputeFreqNew(int *Index, int *Freq, lint *start, int *length, ushort offset, int fourk, lint nS);
 
 #endif
